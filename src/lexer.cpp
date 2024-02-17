2eb06c24c37cda54fc16e1d9500a273867445a2f
Sam Mercer <smercer10@proton.me>
2024-02-17 16:17:32 +0000

    Implement next_char and peek #4

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -5,3 +5,14 @@ void Lexer::abort()
 {
     std::cout << "Something went wrong!\n";
 }
+
+void Lexer::next_char()
+{
+    ++curr_pos;
+    curr_char = (curr_pos >= source.length()) ? '\0' : source[curr_pos];
+}
+
+char Lexer::peek()
+{
+    return (curr_pos + 1 >= source.length()) ? '\0' : source[curr_pos + 1];
+}
