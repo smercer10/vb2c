fdea99c66b000f46e3e3bae5df1d62e80861c828
Sam Mercer <smercer10@proton.me>
2024-02-17 13:54:16 +0000

    Set up basic project structure #1

---
+++ apps/tb2c.cpp
@@ -0,0 +1,8 @@
+#include "tb2c/example.h"
+
+int main()
+{
+    print_greeting("Sam");
+
+    return 0;
+}
---
+++ include/tb2c/example.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+void print_greeting(std::string name);
