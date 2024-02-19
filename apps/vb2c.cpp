8019347ea88bdd0b34dede07b2d4176b9df3b8db
Sam Mercer <smercer10@proton.me>
2024-02-19 17:58:24 +0000

    Read source file from hardcoded path #10

--- apps/vb2c.cpp
+++ apps/vb2c.cpp
@@ -2,14 +2,23 @@
 #include "vb2c/parser.h"
 #include "vb2c/token.h"
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 int main()
 {
-    std::string source = "LET foo = bar * 3 + 2"
-                         "IF foo > 0 THEN      "
-                         "  PRINT \"yes!\"     "
-                         "ENDIF                ";
+    std::ifstream source_file("../../source.vbas");
+
+    if (!source_file.is_open())
+    {
+        std::cerr << "Failed to open source file.\n";
+        return EXIT_FAILURE;
+    }
+
+    std::stringstream buffer;
+    buffer << source_file.rdbuf();
+    std::string source = buffer.str();
 
     Lexer lexer(source);
     Parser parser(lexer);
